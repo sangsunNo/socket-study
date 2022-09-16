9111bf2e74629289c1c1ba28ac919e6064a9dc21
norii97 <mbhb8817@gmail.com>
2022-09-16 17:58:25 +0900

    finish

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.render("test_index");
+    res.render("index");
 })
 
 // key - index
